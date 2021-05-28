372f37458b027f12228f64db494411e422dbc019
Alex <alexbrence10@gmail.com>
2021-05-28 14:15:03 +0200

    Fixed emso error

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("ze-prijavljen/<int:emso>", views.ze_prijavljen , name="ze-prijavljen"),
-    path("prijava-uspesna/<int:emso>", views.prijava_uspesna, name="prijava-uspesna"),
+    path("ze-prijavljen/<str:emso>", views.ze_prijavljen , name="ze-prijavljen"),
+    path("prijava-uspesna/<str:emso>", views.prijava_uspesna, name="prijava-uspesna"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,39 +1,40 @@
-from django.urls import reverse
-from .models import Person
-from django.shortcuts import render
 from .forms import VaccinationSignup
+from .models import Person
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
-def index(response):
-    if response.method == "POST":
-        form = VaccinationSignup(response.POST)
+def index(request):
+    if request.method == "POST":
+        form = VaccinationSignup(data=request.POST)
         if form.is_valid():
+            _email = form.cleaned_data["email"]
+            _emso = form.cleaned_data["emso"]
+            ime = form.cleaned_data["nameInitial"].upper()
+            priimek = form.cleaned_data["surnameInitial"].upper()
+
             try:
-                _email = form.cleaned_data["email"]
-                _emso = form.cleaned_data["emso"]
-                ime = form.cleaned_data["nameInitial"].upper()
-                priimek = form.cleaned_data["surnameInitial"].upper()
                 person = Person.objects.get(emso = _emso)
                 return HttpResponseRedirect(reverse("ze-prijavljen", args=[_emso]))
 
             except Person.DoesNotExist:
-                person = Person(emso = _emso, nameInitial = ime, surnameInitial = priimek, email = _email)
+                person = Person(emso=_emso, nameInitial=ime, surnameInitial=priimek, email=_email)
                 person.save()
                 return HttpResponseRedirect(reverse("prijava-uspesna", args=[_emso]))
             except Exception as e:
                 return HttpResponseRedirect(e)
     else:
         form = VaccinationSignup()
-    return render(response, "main/home.html", {"form":form})
+    return render(request, "main/home.html", {"form": form})
 
 
 def ze_prijavljen(request, emso):
     p = Person.objects.get(emso=emso)
-    return render(request, "main/ze-prijavljen.html", {"person": p, "date": p.signupDate.strftime("%d-%m-%Y %H:%M:%S")})
+    return render(request, "main/ze-prijavljen.html", {"person": p})
 
 
 def prijava_uspesna(request, emso):
     p = Person.objects.get(emso=emso)
-    return render(request, "main/prijava-uspesna.html", {"person": p, "date": p.signupDate.strftime("%d-%m-%Y %H:%M:%S")})
+    return render(request, "main/prijava-uspesna.html", {"person": p})
